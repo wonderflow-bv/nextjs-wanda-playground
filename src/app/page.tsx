1f0fd87cddfd17c9f7d80571f6266581a0081fb6
patrick <patrick.tabarelli@gmail.com>
2023-05-19 13:08:53 +0200

    feat: add some style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,34 @@
 'use client'
 
-// import Image from 'next/image'
-// import styles from './page.module.css'
 import "@wonderflow/react-components/core.css";
 import "@wonderflow/themes";
-import { ResponsiveProvider, Container } from '@wonderflow/react-components'
+import { ResponsiveProvider, Container, Text, Stack } from '@wonderflow/react-components'
 
 export default function Home() {
   return (
     <ResponsiveProvider>
-      <Container>start</Container>
+      <Container dimension="large" padding>
+        <Stack rowGap={32} vPadding={16}>
+          <Stack>
+            <Text variant="subtitle-2">A playgorund based on NextJs@13 and Wanda@12</Text>
+            <Text>
+              <Text as="span" variant="display-4">NextJs + </Text>
+              <Text as="span" variant="display-4" color="danger">Wanda</Text>
+            </Text>
+            <Text variant="tagline-1">Created by Wonderflow Design Team</Text>
+          </Stack>
+
+          <Stack direction="row" fill={false} columnGap={32}>
+            <a href="https://design.wonderflow.ai/get-started/documentation/get-started/introduction" target="_blank">Get Started</a>
+            <a href="https://design.wonderflow.ai/playground" target="_blank">Playground</a>
+            <a href="https://wonderflow-bv.github.io/wanda" target="_blank">Storybook</a>
+            <a href="https://github.com/wonderflow-bv/wanda" target="_blank">Github</a>
+            <a href="https://wonderflow.ai/" target="_blank">Company</a>
+          </Stack>
+
+          <Text variant="body-1">Happy Hacking 🥷</Text>
+        </Stack>
+      </Container>
     </ResponsiveProvider>
   )
 }
